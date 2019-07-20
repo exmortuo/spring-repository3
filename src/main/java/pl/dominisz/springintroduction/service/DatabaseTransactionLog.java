720105ae180aa60cc455ccd0a8bdc175efb014c6
dominisz <dominisz@gmail.com>
2019-07-20 10:37:02 +0200

    Add missing classes

--- src/main/java/pl/dominisz/springintroduction/service/DatabaseTransactionLog.java
+++ src/main/java/pl/dominisz/springintroduction/service/DatabaseTransactionLog.java
@@ -1,4 +1,13 @@
 package pl.dominisz.springintroduction.service;
 
 public class DatabaseTransactionLog implements TransactionLog {
+    @Override
+    public void logChargeResult(ChargeResult result) {
+
+    }
+
+    @Override
+    public void logConnectException(UnreachableException exception) {
+
+    }
 }
--- src/main/java/pl/dominisz/springintroduction/service/PayUCreditCardProcessor.java
+++ src/main/java/pl/dominisz/springintroduction/service/PayUCreditCardProcessor.java
@@ -1,4 +1,10 @@
 package pl.dominisz.springintroduction.service;
 
+import java.math.BigDecimal;
+
 public class PayUCreditCardProcessor implements CreditCardProcessor {
+    @Override
+    public ChargeResult charge(CreditCard creditCard, BigDecimal amount) {
+        return null;
+    }
 }
