b562aa995cee7aa5525895ab109898d7bca71740
dominisz <dominisz@gmail.com>
2019-07-20 11:03:41 +0200

    Receipt implementation

--- src/main/java/pl/dominisz/springintroduction/service/Receipt.java
+++ src/main/java/pl/dominisz/springintroduction/service/Receipt.java
@@ -3,15 +3,40 @@ package pl.dominisz.springintroduction.service;
 import java.math.BigDecimal;
 
 public class Receipt {
-    public static Receipt forSuccessfulCharge(BigDecimal amount) {
-        return null;
-    }
+  private BigDecimal amount;
+  private boolean successful;
+  private String message;
 
-    public static Receipt forDeclinedCharge(String declineMessage) {
-        return null;
-    }
+  public Receipt(BigDecimal amount, boolean successful, String message) {
+    this.amount = amount;
+    this.successful = successful;
+    this.message = message;
+  }
 
-    public static Receipt forSystemFailure(String message) {
-        return null;
-    }
+  public Receipt(BigDecimal amount) {
+    this.amount = amount;
+    this.successful = true;
+    this.message = "";
+  }
+
+  public static Receipt forSuccessfulCharge(BigDecimal amount) {
+    return new Receipt(amount);
+  }
+
+  public static Receipt forDeclinedCharge(String declineMessage) {
+    // TODO można zrobić do tego odpowiedni konstruktor
+    return new Receipt(BigDecimal.ZERO, false, declineMessage);
+  }
+
+  public static Receipt forSystemFailure(String message) {
+    return new Receipt(BigDecimal.ZERO, false, message);
+  }
+
+  public String toString() {
+      if (successful) {
+          return "Charge successful: " + amount;
+      } else {
+          return "Charge failed: " + message;
+      }
+  }
 }
