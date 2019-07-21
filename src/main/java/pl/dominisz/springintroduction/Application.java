e3b6cce69eb7250933b7bc02bd38f1660bf3d11d
dominisz <dominisz@gmail.com>
2019-07-21 11:27:20 +0200

    Add SpringBoot

--- src/main/java/pl/dominisz/springintroduction/Application.java
+++ src/main/java/pl/dominisz/springintroduction/Application.java
@@ -1,28 +1,12 @@
 package pl.dominisz.springintroduction;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import pl.dominisz.springintroduction.model.CreditCard;
-import pl.dominisz.springintroduction.model.Order;
-import pl.dominisz.springintroduction.model.Receipt;
-import pl.dominisz.springintroduction.service.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.math.BigDecimal;
-
-@ComponentScan
+@SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
-    ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
-
-    BillingService billingService = context.getBean(BillingService.class);
-
-    Order order = new Order(BigDecimal.TEN);
-    CreditCard creditCard = new CreditCard("123456", "holder", "123", 1, 2020);
-
-    Receipt receipt = billingService.chargeOrder(order, creditCard);
-
-    System.out.println(receipt);
+    SpringApplication.run(Application.class, args);
   }
 }
