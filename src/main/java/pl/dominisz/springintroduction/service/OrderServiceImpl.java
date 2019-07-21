320fd848a71896950194e53a52d83a4c8696647c
dominisz <dominisz@gmail.com>
2019-07-21 22:14:54 +0200

    Order controller and service

--- src/main/java/pl/dominisz/springintroduction/service/OrderServiceImpl.java
+++ src/main/java/pl/dominisz/springintroduction/service/OrderServiceImpl.java
@@ -2,11 +2,27 @@ package pl.dominisz.springintroduction.service;
 
 import org.springframework.stereotype.Service;
 import pl.dominisz.springintroduction.model.Order;
+import pl.dominisz.springintroduction.model.User;
+import pl.dominisz.springintroduction.repository.UserRepository;
+import pl.dominisz.springintroduction.repository.UserRepositoryImpl;
+
+import java.util.Optional;
 
 @Service
 public class OrderServiceImpl implements OrderService {
+
+  private final OrderRepository orderRepository;
+  private final UserRepository userRepository;
+
+  public OrderServiceImpl(OrderRepository orderRepository, UserRepository userRepository) {
+    this.orderRepository = orderRepository;
+    this.userRepository = userRepository;
+  }
+
   @Override
   public Order createOrderForUser(long id, Order order) {
+    Optional<User> optionalUser = userRepository.findById(id);
+
     return null;
   }
 
